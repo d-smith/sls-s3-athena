ced892e46b1b7def2c2971a253b679e6e23d3b37
d-smith <doug.smith.mail@gmail.com>
2021-08-05 13:57:37 -0700

    Added bucket context extraction and guard condition

--- s3handler.js
+++ s3handler.js
@@ -2,6 +2,16 @@
 
 let handler = async(event) => {
     console.log(JSON.stringify(event));
+    let recs = event.Records;
+    for(rec of recs) {
+        let sourceKey = rec.s3.object.key;
+        console.log(`event source object ${sourceKey} in ${rec.s3.bucket.name}`);
+        if(sourceKey.startsWith('pubrecord/')) {
+            console.log('processing object...');
+        } else {
+            console.log(`ignoring bucket event for ${sourceKey}`);
+        }
+    }
 }
 
 
