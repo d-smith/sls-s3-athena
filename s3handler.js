9c0235b3ed8765cbe3beb0073248d430a7cdded7
d-smith <doug.smith.mail@gmail.com>
2021-08-07 08:37:01 -0700

    added write of parquet data, athena db and table set up

--- s3handler.js
+++ s3handler.js
@@ -2,9 +2,47 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const readline = require('readline');
 
+const through2 = require('through2');
+const stream = require('stream');
+let {ParquetSchema, ParquetTransformer} = require('parquetjs');
+
+const schema = new ParquetSchema({
+    EventId: { type: "UTF8"},
+    EventTime: { type: "UTF8"},
+    EventType: { type: "UTF8"},
+    EventSubject: { type: "UTF8"},
+    EventPayload: { type: "UTF8"}
+});
+
+const uploadStream = ({Bucket,Key}) => {
+    const s3 = new AWS.S3();
+    const pass = new stream.PassThrough();
+    return {
+        writeStream: pass,
+        promise: s3.upload({Bucket, Key, Body: pass}).promise()
+    };
+};
 
 
 let processObject = async(bucket, sourceKey) => {
+
+    //Form destination key
+    let destKey = sourceKey.replace('pubrecord/', 'indexed/');
+    console.log(`write to ${destKey}`);
+
+    //Output pipeline set up
+    var objectStream = through2.obj(function (chunk, encoding, callback){
+        this.push(chunk);
+        callback();
+    });
+
+    const { writeStream, writePromise } = uploadStream({Bucket: bucket, Key: destKey});
+
+    objectStream
+        .pipe(new ParquetTransformer(schema))
+        .pipe(writeStream);
+
+    // Set up source stream read
     const params = {
         Bucket: bucket,
         Key: sourceKey
@@ -17,8 +55,21 @@ let processObject = async(bucket, sourceKey) => {
     });
 
     for await(const line of rl) {
-        console.log(line);
+        console.log(`read line ${line}`);
+
+        let parsed = JSON.parse(line);
+        objectStream.write({
+            EventId: parsed.id,
+            EventTime: parsed.time,
+            EventType: parsed.type,
+            EventSubject: parsed.subject,
+            EventPayload: line
+        });
     }
+
+    objectStream.push(null); //Close the stream
+
+    await writePromise;
 
 };
 
